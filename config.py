dff8bf95f05a94e38bd3632560cf1b96d17ce967
vasilyryabtsev <vasilyryabtsev@gmail.com>
2025-01-14 22:31:06 +0300

    update

--- config.py
+++ config.py
@@ -5,7 +5,12 @@ from dotenv import load_dotenv
 load_dotenv()
 
 # Чтение токена из переменной окружения
-TOKEN = os.getenv("BOT_TOKEN")
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+OPENWEATHER_API_KEY = os.getenv("OPENWEATHER_API_KEY")
 
-if not TOKEN:
+OPENWEATHER_API_URL = 'https://api.openweathermap.org/data/2.5/weather'
+
+if not BOT_TOKEN:
     raise ValueError("Переменная окружения BOT_TOKEN не установлена!")
+if not OPENWEATHER_API_KEY:
+    raise ValueError("Переменная окружения OPENWEATHER_API_KEY не установлена!")
