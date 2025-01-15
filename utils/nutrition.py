5d5f19f8151f956534f6ff1f565f751ead634c5c
vasilyryabtsev <vasilyryabtsev@gmail.com>
2025-01-15 13:04:46 +0300

    update

--- utils/nutrition.py
+++ utils/nutrition.py
@@ -2,6 +2,7 @@ import sys
 import os
 import asyncio
 import aiohttp
+from weather import get_weather_by_city
 
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
@@ -9,7 +10,7 @@ from config import EDAMAM_APP_ID, EDAMAM_APP_KEY, EDAMAM_URL
 
 async def get_calories(ingredient):
     """
-    Получение информации о количестве калорий продукта или напитка.
+    Получение информации о количестве калорий продукта или напитка (ккал).
     """
     params = {
         'app_id': EDAMAM_APP_ID,
@@ -49,7 +50,7 @@ def calculate_activity_coeff(activety_time):
 
 def calculate_calories_norm(sex, weight, height, age, activety_time):
     """
-    Расчет нормы калорий по формуле Харриса-Бенедикта.
+    Расчет нормы калорий в день по формуле Харриса-Бенедикта (ккал).
     
     Параметры:
     weight - кг;
@@ -63,3 +64,20 @@ def calculate_calories_norm(sex, weight, height, age, activety_time):
         return a * (88.362 + (13.397 * weight) + (4.799 * height) - (5.677 * age))
     else:
         return a * (447.593 + (9.247 * weight) + (3.098 * height) - (4.330 * age))
+
+async def calculate_water_norm(weight, activity_time, city):
+    """
+    Расчет нормы потребления воды в день (мл).
+    
+    Параметры:
+    weight - кг.
+    activity_time - время активности в день (минуты).
+    city - город проживания.
+    """
+    temperature = await get_weather_by_city(city)
+    
+    temperature_coeff = 0
+    if temperature >= 25:
+        temperature_coeff = temperature / 25
+    
+    return weight * 30 + 500 * (activity_time / 30) + 500 * temperature_coeff
