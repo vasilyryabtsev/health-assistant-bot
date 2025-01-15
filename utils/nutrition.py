f3c290646b1e659f6c98ee67aa0d09fcf3c55cd6
vasilyryabtsev <vasilyryabtsev@gmail.com>
2025-01-15 12:39:36 +0300

    fix

--- utils/nutrition.py
+++ utils/nutrition.py
@@ -60,4 +60,3 @@ def calculate_calories_norm(sex, weight, height, age, activety_time):
         return a * (88.362 + (13.397 * weight) + (4.799 * height) - (5.677 * age))
     else:
         return a * (447.593 + (9.247 * weight) + (3.098 * height) - (4.330 * age))
-    
