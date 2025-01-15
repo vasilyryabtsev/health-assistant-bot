e053e2123f78dad888b8d112865010dfef3ac3f9
vasilyryabtsev <vasilyryabtsev@gmail.com>
2025-01-15 11:05:14 +0300

    update

--- config.py
+++ config.py
@@ -5,13 +5,23 @@ from dotenv import load_dotenv
 load_dotenv()
 
 # Чтение токена из переменной окружения
-BOT_TOKEN = os.getenv("BOT_TOKEN")
-OPENWEATHER_API_KEY = os.getenv("OPENWEATHER_API_KEY")
+BOT_TOKEN = os.getenv('BOT_TOKEN')
+OPENWEATHER_API_KEY = os.getenv('OPENWEATHER_API_KEY')
+EDAMAM_APP_ID = os.getenv('EDAMAM_APP_ID')
+EDAMAM_APP_KEY = os.getenv('EDAMAM_APP_KEY')
 
 OPENWEATHER_API_URL = 'https://api.openweathermap.org/data/2.5/weather'
 OPENWEATHER_GEO_API_URL = 'http://api.openweathermap.org/geo/1.0/direct'
+EDAMAM_URL = 'https://api.edamam.com/api/nutrition-data'
 
-if not BOT_TOKEN:
-    raise ValueError("Переменная окружения BOT_TOKEN не установлена!")
-if not OPENWEATHER_API_KEY:
-    raise ValueError("Переменная окружения OPENWEATHER_API_KEY не установлена!")
+def validate_token(token):
+    """
+    Проверка наличия токена в переменной окружения.
+    """
+    if not token:
+        raise ValueError(f'Переменная окружения {token} не установлена!')
+    
+validate_token('BOT_TOKEN')
+validate_token('OPENWEATHER_API_KEY')
+validate_token('EDAMAM_APP_ID')
+validate_token('EDAMAM_APP_KEY')
