5b7cf3e83fcb28c63908e60ec00d7708a2ec9984
vasilyryabtsev <vasilyryabtsev@gmail.com>
2025-01-19 22:35:15 +0300

    add logs by logging

--- middlewares/user_logs.py
+++ middlewares/user_logs.py
@@ -1,7 +1,12 @@
+import logging
 from aiogram import BaseMiddleware
 from aiogram.types import Message
 
+# Настройка логирования
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+logger = logging.getLogger(__name__)
+
 class LoggingMiddleware(BaseMiddleware):
     async def __call__(self, handler, event: Message, data: dict):
-        print(f"Получено сообщение от {event.from_user.id}: {event.text}")
+        logger.info(f"Получено сообщение от {event.from_user.id}: {event.text}")
         return await handler(event, data)
