31e9c2939437e86013d5c3ce23ae7e84c11ece7a
vasilyryabtsev <vasilyryabtsev@gmail.com>
2025-01-19 15:17:20 +0300

    add logs

--- bot.py
+++ bot.py
@@ -3,9 +3,13 @@ from aiogram import Bot, Dispatcher
 from config import BOT_TOKEN
 from handlers.new_user import router as new_user_router
 from handlers.user import router as user_router
+from middlewares.user_logs import LoggingMiddleware
+
 
 bot = Bot(token=BOT_TOKEN)
 dp = Dispatcher()
+
+dp.message.middleware(LoggingMiddleware())
 
 dp.include_router(new_user_router)
 dp.include_router(user_router)
