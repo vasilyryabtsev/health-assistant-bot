fb8c81015eb0b087314300dd58a92f02a647e9ea
vasilyryabtsev <vasilyryabtsev@gmail.com>
2025-01-19 14:22:52 +0300

    add docstrings

--- states/registration.py
+++ states/registration.py
@@ -1,6 +1,9 @@
 from aiogram.fsm.state import State, StatesGroup
 
 class UserProfile(StatesGroup):
+    """
+    Профиль юзера.
+    """
     gender = State()
     weight = State()
     height = State()
